34f91b44e6ed430f250921e5c2b187fd013d6460
Mikel Uriz <Mikelok33@gmail.com>
2015-12-24 10:43:44 +0100

    Descargar en usb y listar

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var url = require("url");
 var https = require("https");
 var fs = require('fs');
 
-var org_html = fs.readFileSync('index.html').toString();
+var lastSearch = "";
 
 function callSearch(text, showCallback, response) {
+	lastSearch = text;
 	var options = {
 	  host: 'www.googleapis.com',
 	  port: 443,
@@ -29,26 +30,65 @@ function callSearch(text, showCallback, response) {
 	});
 }
 
+function responseMusic(mp3_file, response) {
+    var stat = fs.statSync(mp3_file);
+
+    response.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fs.createReadStream(filePath);
+    readStream.pipe(response);
+}
+
+function writeHtml(html, response) {
+	response.setHeader('Content-Length', Buffer.byteLength(html));
+	response.setHeader('Content-Type', 'text/html');
+	response.end(html);
+}
+
 function showList(results, response) {
 	var lista = "<table>";
 	for(var i = 0; i < results.items.length; i++) {
 		var videoId = results.items[i].id.videoId;
 		var videoName = results.items[i].snippet.title;
-		lista += "<tr><td>" + videoName + "</td><td><a href=https://www.youtube.es/watch?v=" + videoId + ">Ver</a></td><td><a href=/des/" + videoId + ">Descargar</a></td></tr>"
+		lista += "<tr><td>" + videoName + "</td><td><a href=https://www.youtube.es/watch?v=" + videoId + ">Ver</a></td><td><a href=/sel/" + videoId ">Descargar</a></td></tr>"
 	}
 	lista += "</table>";
 	var out = org_html.replace('##LIST##', lista);
-	response.setHeader('Content-Length', Buffer.byteLength(out));
-	response.setHeader('Content-Type', 'text/html');
-	response.end(out);
+	writeHtml(out, response);
 }
 
-function downloadVideo(videoId, response) {
-	exec("youtube-dl --extract-audio --audio-format mp3 https://www.youtube.es/watch?v=" + videoId);
-	var out = "<script>window.history.back()</script>";
-	response.setHeader('Content-Length', Buffer.byteLength(out));
-	response.setHeader('Content-Type', 'text/html');
-	response.end(out);
+function downloadVideo(where, videoId, response) {
+	exec("youtube-dl --extract-audio --audio-format mp3 https://www.youtube.es/watch?v=" + videoId + " -o \"/mnt/usb/" + where + "%(title)s.%(ext)s\"");
+	if (lastSearch == "") {
+		writeHtml("<script>window.location=/</script>");
+	} else {
+		callSearch(lastSearch, showList, response);
+	}
+}
+
+function showWhereDownload(videoId, response) {
+	var selectFolderHtml = fs.readFileSync('selectFolder.html').toString();
+	var list_dir = fs.readdirSync('/mnt/usb/');
+	var lst = "";
+	for (var i = 0; i < list_dir.length; i++) {
+		lst += "<li><a href=/des/" + videoId + "/" + list_dir[i] + ">" + list_dir[i] + "</a></li>";
+	}
+	selectFolderHtml = selectFolderHtml.replace("##LIST##", lst);
+	writeHtml(selectFolderHtml, response);
+}
+
+function writeListDir(path, response, url) {
+	var selectFolderHtml = fs.readFileSync('selectFolder.html').toString();
+	var list_dir = fs.readdirSync(path);
+	var lst = "";
+	for (var i = 0; i < list_dir.length; i++) {
+		lst += "<li><a href=/" + url + "/" + list_dir[i] + ">" + list_dir[i] + "</a></li>";
+	}
+	selectFolderHtml = selectFolderHtml.replace("##LIST##", lst);
+	writeHtml(selectFolderHtml, response);
 }
 
 
@@ -60,13 +100,19 @@ var server = http.createServer(function (req, res) {
                         if (parts[1] == 'des') {
                         	//console.log("DES " + parts[2]);
                         	downloadVideo(parts[2], res);
+                        } else if (parts[1] == 'sel') {
+                        	showWhereDownload(parts[3], parts[2], res);
                         } else if(parts[1] == 'busc') {
                         	callSearch(parts[2], showList, res);
+                        } else if(parts[1] == 'carp') {
+                        	writeListDir('/mnt/usb/', res, 'list');
+                        } else if(parts[1] == 'list') {
+                        	writeListDir('/mnt/usb/' + parts[2], res, 'mus/' + parts[2]);
+                        } else if(parts[1] == 'mus') {
+                        	responseMusic('/mnt/usb/' + parts[2] + '/' + parts[3], res);
                         } else {
                         	var out = org_html.replace('##LIST##', '');
-                        	res.setHeader('Content-Length', Buffer.byteLength(out));
-                        	res.setHeader('Content-Type', 'text/html');
-                        	res.end(out);
+                        	writeHtml(out, res);
                         }
                         break;
                 default:
