3ea8e6d813f6d22764afec5334289521b310b399
Txuma Uriz <txumauriz@MacBook-Pro-de-Txuma.local>
2015-12-24 17:12:07 +0100

    Fixed error

--- server.js
+++ server.js
@@ -88,8 +88,8 @@ function showList(results, response) {
 
 function downloadVideo(where, videoId, videoName, response) {
 	//console.log("sudo youtube-dl --extract-audio --audio-format mp3 https://www.youtube.es/watch?v=" + videoId + " -o \"/mnt/usb/" + where + "%(title)s.%(ext)s\"");
-	exec("sudo youtube-dl --extract-audio --audio-format mp3 https://www.youtube.es/watch?v=" + videoId + " -o \"" + videoName + ".%(ext)s\"");
-	console.log("sudo youtube-dl --extract-audio --audio-format mp3 https://www.youtube.es/watch?v=" + videoId + " -o \"" + videoName + ".%(ext)s\"");
+	exec("sudo youtube-dl --extract-audio --audio-format mp3 https://www.youtube.es/watch?v=" + videoId + " -o \"" + videoName.replace('+',' ') + ".%(ext)s\"");
+	console.log("sudo youtube-dl --extract-audio --audio-format mp3 https://www.youtube.es/watch?v=" + videoId + " -o \"" + videoName.replace('+', ' ') + ".%(ext)s\"");
 	//exec("sudo youtube-dl --extract-audio --audio-format mp3 https://www.youtube.es/watch?v=" + videoId);
 	if (lastSearch == "") {
 		writeHtml("<script>window.location=/</script>", response);
